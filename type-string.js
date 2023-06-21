7707b873afeaac7a8b2a66462e2c2f4d83e256a8
Henrique Santos <henrique30hs@gmail.com>
2023-06-21 15:07:30 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo";
+const texto2 = 'Olá mundo';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Henrique";
+
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+
+// template string ou template literal
