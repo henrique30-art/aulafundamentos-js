427af76540fb34dc64c91de36e39984c5be9a1b8
Henrique Santos <henrique30hs@gmail.com>
2023-07-25 18:29:21 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado 
+// booleanos
+
+// conerversão implícita
+//const numero = 456;
+//const numeroString = "456";
+
+//console.log(numero == numeroString)
+
+//coversão explícita
+
+// Number()
+// String()
+
+const numero = 456;
+const numeroString = Number("456");
+
+console.log(numero + numeroString)
